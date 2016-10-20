9d187766c0dc8f63eb707be8b54d8fe1440327da
Krister <krister.torsvik@gmail.com>
2016-10-20 21:19:09 +0200

    angular2 examples

--- block-scope-examples.js
+++ block-scope-examples.js
@@ -1,10 +1,9 @@
 // - the blocked scope makes it so that variables cant be accessed outside
 //   the functions scope.
-
-const add3 = (y) => {
-  let x = 3;
-  y += x;
-  console.log(y); // - this is valid
-}
-add3(4)
+var add3 = function (y) {
+    var x = 3;
+    y += x;
+    console.log(y); // - this is valid
+};
+add3(4);
 //console.log(y);  - this is not valid
