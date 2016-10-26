0f83418326bb0941c324fbde045c85766b7e5be7
Krister <krister.torsvik@gmail.com>
2016-10-26 15:46:00 +0200

    angular 2 examples

--- events-examples.ts
+++ events-examples.ts
@@ -15,3 +15,32 @@ export class AppComponent {
     this.visible = !this.visible;
   };
 }
+
+// *ngFor
+import { Component } from 'angular/core';
+
+@Component ({
+  selector: 'app-root',
+  // magic variables:
+  // - first, last, odd, even are boolean
+  // - index is numeric
+  template: `<ol>
+      <li *ngFor="let item of list; let o = odd;">
+      <span>({{ item.id }}) isOdd: {{ o }}</span>
+      <span>({{ item.id }}) isEven: {{ e }}</span>  // - just change 'let o = odd' to 'let e = even')
+      <span>({{ item.id }}) isFirst: {{ f }}</span> // - just change 'let o = odd' to 'let f = first')
+      <span>({{ item.id }}) index: {{ i }}</span>  // - just change 'let o = odd' to 'let i = index')
+      <span>{{ item.value }}</span>
+      </li>
+    </ol>`,
+  styles: [``]
+})
+export class AppComponent {
+list = [
+    {id: 0, value: 'zero'},
+    {id: 1, value: 'one'},
+    {id: 2, value: 'two'},
+    {id: 3, value: 'three'},
+    {id: 4, value: 'four'},
+  ];
+}
